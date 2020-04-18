888903030db5038127f5e32f3eee72f257a8ea42
Satish Marutham <smarutham@gmail.com>
2020-04-18 22:14:03 +0530

    Second Commit

--- CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRestController.java
+++ CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRestController.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class CategoryRestController {
 
 	@Autowired
-	CategoryRepository categoryRepository;
-	
+	CategoryRepository categoryRepositoryJPA;
 
 }
