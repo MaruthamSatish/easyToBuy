21a29b89393ecc3b29742379ca64b622eaa7aa82
Satish Marutham <smarutham@gmail.com>
2020-05-07 19:19:13 +0530

    Kubernates

--- CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRepository.java
+++ CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRepository.java
@@ -5,6 +5,7 @@ package com.easttobuy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  * 
  */
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(path = "categorys", collectionResourceRel = "categorys")
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 	Category findByCategoryId(@Param("categoryId") Integer categoryId);
 
--- CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRestController.java
+++ CATEGORY_SERVICE_API/src/main/java/com/easttobuy/CategoryRestController.java
@@ -3,18 +3,6 @@
  */
 package com.easttobuy;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,39 +14,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/categoryAPI")
 public class CategoryRestController {
 
-	@Autowired
-	CategoryRepository categoryRepository;
+	/*
+	 * @Autowired CategoryRepository categoryRepository;
+	 */
 
-	@GetMapping("/categories/{categoryId}")
-	public ResponseEntity<Category> getByCategoryId(@PathVariable("categoryId") Integer categoryId) {
-		Category categoryData = categoryRepository.findByCategoryId(categoryId);
-     if (categoryData != null) {
-			return new ResponseEntity<Category>(categoryData, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
-	
-	@GetMapping("/categories")
-	public List<Category> getAllCategory() {
-		List<Category> getAllCategories=new ArrayList<>();
-		try {
-			categoryRepository.findAll().forEach(category->getAllCategories.add(category));
-			System.out.println(getAllCategories);
-		
-		}
-		catch (Exception e) {
-			
-			// TODO: handle exception
-		}
-		return getAllCategories;
-		
-	}
-	@PostMapping(value = "/categories")
-	private ResponseEntity<Category> addCategory(@Valid @RequestBody Category category) {
-	    return ResponseEntity
-	            .ok()
-	            .body(this.categoryRepository.save(category));
-	}
-
+	/*
+	 * @GetMapping("/categories/{categoryId}") public ResponseEntity<Category>
+	 * getByCategoryId(@PathVariable("categoryId") Integer categoryId) { Category
+	 * categoryData = categoryRepository.findByCategoryId(categoryId); if
+	 * (categoryData != null) { return new ResponseEntity<Category>(categoryData,
+	 * HttpStatus.OK); } else { return new ResponseEntity<>(HttpStatus.NOT_FOUND); }
+	 * }
+	 * 
+	 * @GetMapping("/categories") public List<Category> getAllCategory() {
+	 * List<Category> getAllCategories=new ArrayList<>(); try {
+	 * categoryRepository.findAll().forEach(category->getAllCategories.add(category)
+	 * ); System.out.println(getAllCategories);
+	 * 
+	 * } catch (Exception e) {
+	 * 
+	 * // TODO: handle exception } return getAllCategories;
+	 * 
+	 * }
+	 * 
+	 * @PostMapping(value = "/categories") private ResponseEntity<Category>
+	 * addCategory(@Valid @RequestBody Category category) { return ResponseEntity
+	 * .ok() .body(this.categoryRepository.save(category)); }
+	 */
 }
